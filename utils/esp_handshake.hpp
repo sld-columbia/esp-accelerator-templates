dbf296f61b1f205d302d654dcdc6ecc270e880f8
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2020-04-23 15:17:49 -0400

    Refactoring handshake (simpler API for both p2p/connections)

--- utils/esp_handshake.hpp
+++ utils/esp_handshake.hpp
@@ -18,29 +18,10 @@ class handshake_t;
 class handshake_req_t
 {
 public:
-    // Friend zone
-    friend class handshake_t;
 
     // Constructor
     handshake_req_t(sc_module_name name) : __req(name) { }
 
-    // Clock and reset binding
-    template<typename CLK, typename RST>
-    void clk_rst(CLK& clk, RST& rst) {
-        //__req.clk_rst(clk,rst);
-    }
-
-    // Reset method
-    void reset_req() {
-        __req.reset_sync_out();
-    }
-
-    // Req method
-    void req() {
-        __req.sync_out();
-    }
-
-private:
     // Request channel
     p2p_sync<>::out<> __req;
 
@@ -50,31 +31,11 @@ private:
 
 class handshake_ack_t
 {
-
 public:
-    // Friend zone
-    friend class handshake_t;
 
     // Constructor
     handshake_ack_t(sc_module_name name) : __ack(name) { }
 
-    // Clock and reset binding
-    template<typename CLK, typename RST>
-    void clk_rst(CLK& clk, RST& rst) {
-        //__ack.clk_rst(clk,rst);
-    }
-
-    // Reset method
-    void reset_ack() {
-        __ack.reset_sync_in();
-    }
-
-    // Ack method
-    void ack() {
-        __ack.sync_in();
-    }
-
-private:
     // Ack channel
     p2p_sync<>::in<> __ack;
 };
@@ -91,8 +52,8 @@ public:
     handshake_t(sc_module_name name) :
 #if defined(__MATCHLIB_CONNECTIONS__)
 #else
-        req(std::string(name).append("_req").c_str()),
-        ack(std::string(name).append("_ack").c_str()),
+        __req(std::string(name).append("_req").c_str()),
+        __ack(std::string(name).append("_ack").c_str()),
 #endif
         __channel(name) {
 #if defined(__MATCHLIB_CONNECTIONS__)
@@ -100,10 +61,10 @@ public:
 //            ack.__ack(__channel);
 #else
 #warning "Catapult HLS does not suppor the binding of Legacy P2P channels in the same SC_MODULE."
-            req.__req(__channel);
-            ack.__ack(__channel);
+            __req.__req(__channel);
+            __ack.__ack(__channel);
 #endif
-        }
+    }
 
 #if defined(__MATCHLIB_CONNECTIONS__)
 
@@ -123,12 +84,27 @@ public:
         __channel.Push(true);
     }
 #else
-    // Req and ack
-    handshake_req_t req;
-    handshake_ack_t ack;
-#endif
 
-private:
+    void reset_req() {
+        __req.__req.reset_sync_out();
+    }
+
+    void reset_ack() {
+        __ack.__ack.reset_sync_in();
+    }
+
+    void req() {
+        __req.__req.sync_out();
+    }
+
+    void ack() {
+        __ack.__ack.sync_in();
+    }
+
+    // Req and ack
+    handshake_req_t __req;
+    handshake_ack_t __ack;
+#endif
 
     // Channel
 #if defined(__MATCHLIB_CONNECTIONS__)
