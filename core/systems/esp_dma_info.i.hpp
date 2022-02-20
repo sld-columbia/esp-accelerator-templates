f2970474d99d14e37b2d8c9a811cfb6cf52d78e8
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2022-02-20 01:20:17 -0500

    Update Copyright year to 2022

--- core/systems/esp_dma_info.i.hpp
+++ core/systems/esp_dma_info.i.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// Copyright (c) 2011-2022 Columbia University, System Level Design Group
 // SPDX-License-Identifier: MIT
 
 inline dma_info_t& dma_info_t::operator=(const dma_info_t &other)
