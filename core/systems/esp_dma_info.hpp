4285216a6d29781a624dd8b338653f5fa6ae0bb7
Joseph Zuckerman <jzuck@cs.columbia.edu>
2024-03-24 11:20:12 -0400

    add new user fields to read and write ctrl channels

--- core/systems/esp_dma_info.hpp
+++ core/systems/esp_dma_info.hpp
@@ -33,16 +33,19 @@ class dma_info_t
         // Length
         sc_dt::sc_bv<3> size;
 
+        // User
+        sc_dt::sc_bv<5> user;
+
         // Constructors
 
         dma_info_t()
-            : index(0), length(0), size(SIZE_WORD) { }
+            : index(0), length(0), size(SIZE_WORD), user(0) { }
 
-        dma_info_t(uint32_t i, uint32_t l, sc_dt::sc_bv<3> s)
-            : index(i), length(l), size(s) { }
+        dma_info_t(uint32_t i, uint32_t l, sc_dt::sc_bv<3> s, sc_dt::sc_bv<5> u)
+            : index(i), length(l), size(s), user(u) { }
 
         dma_info_t(const dma_info_t &other)
-            : index(other.index), length(other.length), size(other.size) { }
+            : index(other.index), length(other.length), size(other.size), user(other.user) { }
 
         // Operators
 
--- core/systems/esp_dma_info.i.hpp
+++ core/systems/esp_dma_info.i.hpp
@@ -6,6 +6,7 @@ inline dma_info_t& dma_info_t::operator=(const dma_info_t &other)
     index = other.index;
     length = other.length;
     size = other.size;
+    user = other.user;
     return *this;
 }
 
@@ -13,14 +14,16 @@ inline bool dma_info_t::operator==(const dma_info_t &rhs) const
 {
     return ((rhs.index == index)
             && (rhs.length == length)
-            && (rhs.size == size));
+            && (rhs.size == size)
+            && (rhs.user == user));
 }
 
 inline ostream& operator<<(ostream& os, dma_info_t const &dma_info)
 {
     os << "{" << dma_info.index  << ","
               << dma_info.length  << ","
-              << dma_info.size << "}";
+              << dma_info.size << ","
+              << dma_info.user << "}";
     return os;
 }
 
@@ -33,5 +36,7 @@ inline void sc_trace(sc_trace_file *tf, const dma_info_t &v, const std::string &
     sc_trace(tf, v.length, sstm_c.str());
     sstm_c << name << ".size";
     sc_trace(tf, v.size, sstm_c.str());
+    sstm_c << name << ".user";
+    sc_trace(tf, v.user, sstm_c.str());
 }
 
